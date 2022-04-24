f7ba7c50d40e507a84a105b59816f47562591622
Tim Parker <Tim.ParkerD@gmail.com>
2022-04-24 14:35:32 -0500

    refactor: improve defaults for CLI main process

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -5,6 +5,8 @@ import argparse
 import logging
 import sys
 import time
+from os import getcwd
+from os.path import realpath
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
 import click
 {%- endif %}
@@ -24,31 +26,35 @@ from . import __version__, log
 {%- if cookiecutter.command_line_interface|lower == 'argparse' %}
 def main():
     """Console script for {{cookiecutter.project_slug}}."""
-    description = "Console script for {{cookiecutter.project_slug}}"
-    parser = argparse.ArgumentParser(description=description,formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-v", "--verbose", action="store_true", help="Increase output verbosity")
-    parser.add_argument("-V", "--version", action="version", version=f'%(prog)s {__version__} ')
-    parser.add_argument("-f", "--force", action="store_true", help="Force file creation. Overwrite any existing files.")
-    parser.add_argument("-n", "--dry-run", dest='dryrun', action="store_true", help="Perform a trial run. Only console and syslog are updated.")
-    parser.add_argument("path", metavar='PATH', type=str, nargs=1, help='Input directory to process')
-    args = parser.parse_args()
+    try:
+        description = "Console script for {{cookiecutter.project_slug}}"
+        parser = argparse.ArgumentParser(description=description,formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+        parser.add_argument("-V", "--version", action="version", version=f'%(prog)s {__version__} ')
+        parser.add_argument("-f", "--force", action="store_true", help="overwrite existing files without prompting")
+        parser.add_argument("-n", "--dry-run", dest='dryrun', action="store_true", help="perform a trial run with no changes made")
+        parser.add_argument("path", metavar='PATH', type=str, nargs="*", default=realpath(getcwd()), help='path containing input data')
+        args = parser.parse_args()
 
-    log.configure(module_name = 'main',**vars(args))
+        log.configure(module_name = 'main',**vars(args))
 
-    if args.dryrun:
-        logging.info(f"DRY-RUN MODE ENABLED")
+        if args.dryrun:
+            logging.info(f"DRY-RUN MODE ENABLED")
 
-    # Disable progress bars if verbose mode enabled
-    if args.verbose:
-        args.progress = False
-        
-    start_time = time.perf_counter()
-    # returncode = do_work()  # TODO: replace me
-    logging.info(f'Total execution time: {time.perf_counter() - start_time} seconds')
-    return returncode
+        # Disable progress bars if verbose mode enabled
+        if args.verbose:
+            args.progress = False
+            
+        start_time = time.perf_counter()
+        returncode = 0
+        # returncode = do_work()  # TODO: replace me
+        logging.info(f'Total execution time: {time.perf_counter() - start_time} seconds')
+        return returncode
+    except Exception as e:
+        logging.exception(e)
+        raise
 
 {%- endif %}
-
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
